5229f1021e03a5e8b736a2c76e1e4bc13c22058d
Nick Leach <njleach.1@gmail.com>
2021-03-20 15:58:47 +0000

    change LMoment fitting functions to object oriented versions

--- mystatsfunctions/LMoments.py
+++ mystatsfunctions/LMoments.py
@@ -6,6 +6,10 @@ import scipy as sp
 
 # Method of L-moments for fitting a number of set statistical distributions
 
+# TODO: make sure the override functions are compatible with the shape parameter fits (ie. setting GEV CDF to 1 above max threshold)
+# TODO: add gamma distribution class
+# TODO: add quantile function method
+
 ## auxiliary functions ##
 def b_r(r,a):
     
@@ -29,176 +33,231 @@ def lmom_to_3(a):
 
 ## distribution fit functions ##
 
-def fit_gev_pwm(a):
+class gev:
+    ## attributes
     
-    """
-    Fits the (3) parameters of a GEV distribution over the first dimension of a
-    
-    a : np.ndarray
-    """
-    
-    a_sort = np.sort(a,axis=0)
-    
-    b = [b_r(i,a_sort) for i in np.arange(3)]
-    
-    c = (2*b[1]-b[0])/(3*b[2]-b[0]) - np.log(2)/np.log(3)
-    
-    k = 7.8590*c + 2.9554*c**2
-    
-    a = ( 2*b[1]-b[0] )*k / ( sp.special.gamma(1+k) * (1-2**(-k)) )
-    
-    X = b[0] + a*( sp.special.gamma(1+k) - 1 ) / k
-    
-    return k,X,a
+    def __init__(self, k=None, X=None, a=None):
+        self.k = k
+        self.X = X
+        self.a = a
+        
+    def fit(self, x):
+        
+        """
+        Fits the (3) parameters of a GEV distribution over the first dimension of x
 
-def get_gev(x,params,function='cdf'):
+        x : np.ndarray
+        """
+
+        x_sort = np.sort(x,axis=0)
+
+        l = lmom_to_3(x_sort)
+
+        c = 2/(3+l[2]/l[1]) - np.log(2)/np.log(3)
+
+        k = 7.8590*c + 2.9554*c**2
+
+        a = l[1]*k / ( sp.special.gamma(1+k) * (1-2**(-k)) )
+
+        X = l[0] + a*( sp.special.gamma(1+k) - 1 ) / k
+
+        self.k=k
+        self.X=X
+        self.a=a
+
+    def pdf(self, x):
+        
+        """
+        Returns the pdf of a GEV based on the set parameters. 
+        Raises exception if parameters not set or fit to data.
+        """
+        
+        if any(value is None for value in self.__dict__.values()):
+            raise TypeError('GEV parameters not fully set.')
+        
+        pdf = (1/self.a) * ( 1-self.k*( (x-self.X)/self.a ) )**( (1/self.k)-1 ) * np.exp( -1*(1-self.k*( (x-self.X)/self.a) )**(1/self.k) )
+        # set values above the GEV limit to be 1 rather than undefined
+        return np.where(np.isnan(pdf),0,pdf)
+
+    def cdf(self, x):
+        
+        """
+        Returns the cdf of a GEV based on the set parameters. 
+        Raises exception if parameters not set or fit to data.
+        """
+        
+        if any(value is None for value in self.__dict__.values()):
+            raise TypeError('GEV parameters not fully set.')
+        
+        cdf = np.exp(-1*(1-self.k*((x-self.X)/self.a))**(1/self.k))
+        # set values above the GEV limit to be 1 rather than undefined
+        return np.where(np.isnan(cdf),1,cdf)
     
-    """
-    Returns either cdf or pdf of GEV distribution, based on parameter array 'params'
     
-    x : np.ndarray
+class glo:
+    ## attributes
     
-    params : np.ndarray, identical to the output from 'fit_gev_pwm'
+    def __init__(self, k=None, X=None, a=None):
+        self.k = k
+        self.X = X
+        self.a = a
+        
+    def fit(self, x):
+        
+        """
+        Fits the (3) parameters of a GEV distribution over the first dimension of x
+
+        x : np.ndarray
+        """
+
+        x_sort = np.sort(x,axis=0)
+
+        l = lmom_to_3(x_sort)
+
+        k = -l[2]/l[1]
     
-    function : 'cdf' or 'pdf'
-    """
-    
-    k,X,a = params
-    
-    if function=='cdf':
-        cdf = np.exp(-1*(1-k*((x-X)/a))**(1/k))
-        cdf[np.isnan(cdf)] = 1 # set values above the GEV limit to be 1 rather than undefined
-        return cdf
-    
-    if function=='pdf':
-        pdf = (1/a) * ( 1-k*( (x-X)/a ) )**( (1/k)-1 ) * np.exp( -1*(1-k*( (x-X)/a) )**(1/k) )
-        pdf[np.isnan(pdf)] = 0 # set values above the GEV limit to be 1 rather than undefined
+        a = l[1] / (sp.special.gamma(1+k)*sp.special.gamma(1-k))
+
+        X = l[0]+(l[1]-a)/k
+
+        self.k=k
+        self.X=X
+        self.a=a
+
+    def pdf(self, x):
+        
+        """
+        Returns the pdf of a GL based on the set parameters. 
+        Raises exception if parameters not set or fit to data.
+        """
+        
+        if any(value is None for value in self.__dict__.values()):
+            raise TypeError('GLo parameters not fully set.')
+        
+        pdf = (1-self.k*(x-self.X)/self.a)**(1/self.k-1) / ( self.a * ( 1 + (1-self.k*(x-self.X)/self.a)**(1/self.k) )**2 )
         return pdf
 
-def fit_glo_pwm(a):
-    
-    """
-    Fits the (3) parameters of a GL distribution over the first dimension of a
-    
-    a : np.ndarray
-    """
-    
-    a_sort = np.sort(a,axis=0)
-    
-    l = lmom_to_3(a_sort)
-    
-    k = -l[2]/l[1]
-    
-    a = l[1] / (sp.special.gamma(1+k)*sp.special.gamma(1-k))
-    
-    X = l[0]+(l[1]-a)/k
-    
-    return k,X,a
-
-def get_glo(x,params,function='cdf'):
-    
-    """
-    Returns either cdf or pdf of GL distribution, based on parameter array 'params'
-    
-    x : np.ndarray
-    
-    params : np.ndarray, identical to the output from 'fit_glo_pwm'
-    
-    function : 'cdf' or 'pdf'
-    """
-    
-    k,X,a = params
-    
-    if function=='cdf':
-        cdf = 1/( 1 + (1-k*(x-X)/a)**(1/k) )
+    def cdf(self, x):
+        
+        """
+        Returns the cdf of a GL based on the set parameters. 
+        Raises exception if parameters not set or fit to data.
+        """
+        
+        if any(value is None for value in self.__dict__.values()):
+            raise TypeError('GLo parameters not fully set.')
+        
+        cdf = 1/( 1 + (1-self.k*(x-self.X)/self.a)**(1/self.k) )
         return cdf
+
+
+class gpd:
+    ## attributes
     
-    if function=='pdf':
-        pdf = (1-k*(x-X)/a)**(1/k-1) / ( a * ( 1 + (1-k*(x-X)/a)**(1/k) )**2 )
+    def __init__(self, k=None, X=None, a=None):
+        self.k = k
+        self.X = X
+        self.a = a
+        
+    def fit(self, x):
+        
+        """
+        Fits the (3) parameters of a GP distribution over the first dimension of x
+
+        x : np.ndarray
+        """
+
+        x_sort = np.sort(x,axis=0)
+
+        l = lmom_to_3(x_sort)
+
+        k = ( 1-3*l[2]/l[1] ) / ( 1+l[2]/l[1] )
+    
+        a = l[1]*(1+k)*(2+k)
+
+        X = l[0] - l[1]*(2+k)
+
+        self.k=k
+        self.X=X
+        self.a=a
+
+    def pdf(self, x):
+        
+        """
+        Returns the pdf of a GPD based on the set parameters. 
+        Raises exception if parameters not set or fit to data.
+        """
+        
+        if any(value is None for value in self.__dict__.values()):
+            raise TypeError('GPD parameters not fully set.')
+        
+        pdf = (1/self.a) * ( 1 - self.k*(x-self.X)/self.a )**(1/self.k-1)
+        return np.where(x>X , pdf , 0)
+
+    def cdf(self, x):
+        
+        """
+        Returns the cdf of a GPD based on the set parameters. 
+        Raises exception if parameters not set or fit to data.
+        """
+        
+        if any(value is None for value in self.__dict__.values()):
+            raise TypeError('GPD parameters not fully set.')
+        
+        cdf = 1 - ( 1 - self.k*(x-self.X)/self.a )**(1/self.k)
+        return np.where(x>X , cdf , 0)
+
+    
+class norm:
+    ## attributes
+    
+    def __init__(self, X=None, a=None):
+        self.X = X
+        self.a = a
+        
+    def fit(self, x):
+        
+        """
+        Fits the (2) parameters of a normal distribution over the first dimension of x
+
+        x : np.ndarray
+        """
+
+        x_sort = np.sort(x,axis=0)
+
+        l = lmom_to_3(x_sort)
+
+        a = np.pi**(1/2) * l[1]
+    
+        X = l[0]
+
+        self.X=X
+        self.a=a
+
+    def pdf(self, x):
+        
+        """
+        Returns the pdf of a normal based on the set parameters. 
+        Raises exception if parameters not set or fit to data.
+        """
+        
+        if any(value is None for value in self.__dict__.values()):
+            raise TypeError('norm parameters not fully set.')
+            
+        y = (x-X)/a
+        pdf = ( 1/( self.a * np.sqrt(2*np.pi) ) ) * np.exp( -y**2 / 2 )
         return pdf
 
-def fit_gpd_pwm(a):
-    
-    """
-    Fits the (3) parameters of a GL distribution over the first dimension of a
-    
-    a : np.ndarray
-    """
-    
-    a_sort = np.sort(a,axis=0)
-    
-    l = lmom_to_3(a_sort)
-    
-    k = ( 1-3*l[2]/l[1] ) / ( 1+l[2]/l[1] )
-    
-    a = l[1]*(1+k)*(2+k)
-    
-    X = l[0] - l[1]*(2+k)
-    
-    return k,X,a
-
-def get_gpd(x,params,function='cdf'):
-    
-    """
-    Returns either cdf or pdf of GL distribution, based on parameter array 'params'
-    
-    x : np.ndarray
-    
-    params : np.ndarray, identical to the output from 'fit_gpd_pwm'
-    
-    function : 'cdf' or 'pdf'
-    """
-    
-    k,X,a = params
-    
-    if function=='cdf':
-        cdf = 1 - ( 1 - k*(x-X)/a )**(1/k)
-        cdf = np.where(x>X , cdf , 0) # if x < location parameter, density is zero
-        return cdf
-    
-    if function=='pdf':
-        pdf = (1/a) * ( 1 - k*(x-X)/a )**(1/k-1)
-        pdf = np.where(x>X , pdf , 0) # if x < location parameter, density is zero
-        return pdf
-
-def fit_norm_pwm(a):
-    
-    """
-    Fits the (2) parameters of a normal distribution over the first dimension of a
-    
-    a : np.ndarray
-    """
-    
-    a_sort = np.sort(a,axis=0)
-    
-    l = lmom_to_3(a_sort)
-    
-    s = np.pi**(1/2) * l[1]
-    
-    m = l[0]
-    
-    return m,s
-
-def get_norm(x,params,function='cdf'):
-    
-    """
-    Returns either cdf or pdf of normal distribution, based on parameter array 'params'
-    
-    x : np.ndarray
-    
-    params : np.ndarray, identical to the output from 'fit_norm_pwm'
-    
-    function : 'cdf' or 'pdf'
-    """
-    
-    m,s = params
-    
-    y = (x-m)/s
-    
-    if function=='cdf':
+    def cdf(self, x):
+        
+        """
+        Returns the cdf of a normal based on the set parameters. 
+        Raises exception if parameters not set or fit to data.
+        """
+        
+        if any(value is None for value in self.__dict__.values()):
+            raise TypeError('norm parameters not fully set.')
+        
+        y = (x-X)/a
         cdf = (1/2) * (1 + sp.special.erf(y/np.sqrt(2)))
         return cdf
-    
-    if function=='pdf':
-        pdf = ( 1/( s * np.sqrt(2*np.pi) ) ) * np.exp( -y**2 / 2 )
-        return pdf
